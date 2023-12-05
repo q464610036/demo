b7b75855c85ef062970e89cf0371b9f136749a82
chenmengfei <chenmengfei@vv.cn>
2023-12-05 09:46:01 +0800

    邮箱地址可配

--- lineage2/src/main/java/com/cn/module/monitor/MonitorMain.java
+++ lineage2/src/main/java/com/cn/module/monitor/MonitorMain.java
@@ -1,6 +1,15 @@
 package com.cn.module.monitor;
 
+
+import com.cn.common.util.StringUtil;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class MonitorMain {
     final static ArrayList<String> emailList = new ArrayList<>();
@@ -8,8 +17,23 @@ public class MonitorMain {
     final static Double similarity = 0.9D;
 
     static {
-        emailList.add("464610036@qq.com");
-        emailList.add("38755065@qq.com");
+//        emailList.add("464610036@qq.com");
+////        emailList.add("38755065@qq.com");
+//        emailList.add("344542878@qq.com");
+        InputStream inputStream = MonitorMain.class.getResourceAsStream("/main.properties");
+        Properties properties=new Properties();
+        try {
+            properties.load(inputStream);
+            System.out.println("email="+properties.getProperty("email"));
+            String[] emails = properties.getProperty("email").split(",");
+            for (String email : emails) {
+                if (!StringUtil.isEmpty(email)) {
+                    emailList.add(email);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
