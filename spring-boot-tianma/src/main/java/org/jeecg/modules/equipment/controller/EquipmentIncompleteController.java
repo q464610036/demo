7c9492353a593ed45dd4fe1e83f772bf36baab01
chenmengfei1986 <464610036@qq.com>
2024-08-02 10:08:19 +0800

    无

--- spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/controller/EquipmentIncompleteController.java
+++ spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/controller/EquipmentIncompleteController.java
@@ -1,7 +1,18 @@
 package org.jeecg.modules.equipment.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.jeecg.modules.common.entity.Result;
+import org.jeecg.modules.common.page.PageInfo;
+import org.jeecg.modules.equipment.dto.EquipmentIncompleteQueryPageDto;
+import org.jeecg.modules.equipment.vo.EquipmentIncompleteListVo;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,8 +22,20 @@ import org.springframework.stereotype.Controller;
  * @author baomidou
  * @since 2024-07-24
  */
-@Controller
+@RestController
+@Api(tags = "设备保养-残件管理")
 @RequestMapping("/equipment/equipmentIncomplete")
 public class EquipmentIncompleteController {
 
+    @ApiOperation("查询分页")
+    @PostMapping("/getPage")
+    public Result<PageInfo<EquipmentIncompleteListVo>> getPage(@RequestBody @Validated EquipmentIncompleteQueryPageDto dto){
+        return null;
+    }
+
+    @ApiOperation("查询列表")
+    @PostMapping("/getList")
+    public Result<List<EquipmentIncompleteListVo>> getList(@RequestBody @Validated EquipmentIncompleteQueryPageDto dto){
+        return null;
+    }
 }
