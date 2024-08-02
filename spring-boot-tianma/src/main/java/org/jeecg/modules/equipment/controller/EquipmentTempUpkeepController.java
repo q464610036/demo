7d1a5f2105bf575da4eb5d1e19770c5c991d2acf
chenmengfei1986 <464610036@qq.com>
2024-08-02 16:46:11 +0800

    无

--- spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/controller/EquipmentTempUpkeepController.java
+++ spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/controller/EquipmentTempUpkeepController.java
@@ -1,7 +1,18 @@
 package org.jeecg.modules.equipment.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.jeecg.modules.common.entity.Result;
+import org.jeecg.modules.common.page.PageInfo;
+import org.jeecg.modules.equipment.dto.EquipmentTempUpkeepQueryPageDto;
+import org.jeecg.modules.equipment.dto.EquipmentTempUpkeepSaveDto;
+import org.jeecg.modules.equipment.vo.EquipmentTempUpkeepListVo;
+import org.jeecg.modules.equipment.vo.EquipmentTempUpkeepSaveResultVo;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -11,8 +22,19 @@ import org.springframework.stereotype.Controller;
  * @author baomidou
  * @since 2024-07-24
  */
-@Controller
+@RestController
+@Api(tags = "设备保养-临时保养管理")
 @RequestMapping("/equipment/equipmentTempUpkeep")
 public class EquipmentTempUpkeepController {
+    @ApiOperation("查询分页")
+    @PostMapping("/getPage")
+    public Result<PageInfo<EquipmentTempUpkeepListVo>> getPage(@RequestBody @Validated EquipmentTempUpkeepQueryPageDto dto){
+        return null;
+    }
 
+    @ApiOperation("添加")
+    @PostMapping("/add")
+    public Result<EquipmentTempUpkeepSaveResultVo> add(@RequestBody @Validated EquipmentTempUpkeepSaveDto dto){
+        return null;
+    }
 }
