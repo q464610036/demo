6bf5c2874d3364ace1e26cabff4ebd7a4a3c121d
chenmengfei1986 <464610036@qq.com>
2024-08-05 13:31:19 +0800

    无

--- spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/controller/EquipmentBorrowController.java
+++ spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/controller/EquipmentBorrowController.java
@@ -1,7 +1,13 @@
 package org.jeecg.modules.equipment.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.jeecg.modules.common.entity.Result;
+import org.jeecg.modules.equipment.vo.EquipmentOrderDetailVo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -11,8 +17,14 @@ import org.springframework.stereotype.Controller;
  * @author baomidou
  * @since 2024-08-05
  */
-@Controller
+@RestController
+@Api(tags = "设备借机-借机申请")
 @RequestMapping("/equipment/equipmentBorrow")
 public class EquipmentBorrowController {
 
+    @ApiOperation(value = "查询详情", notes = "非待审批单详情入口")
+    @GetMapping("/detail/{id}")
+    public Result<EquipmentOrderDetailVo> detail(@PathVariable String id){
+        return null;
+    }
 }
---
+++ spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/vo/EquipmentBorrowDetailVo.java
@@ -0,0 +1,87 @@
+package org.jeecg.modules.equipment.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * <p>
+ * 借机表
+ * </p>
+ *
+ * @author baomidou
+ * @since 2024-08-05
+ */
+@Data
+public class EquipmentBorrowDetailVo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty("主键id")
+    private String id;
+
+    @ApiModelProperty("借机单号")
+    private String borrowNo;
+
+    @ApiModelProperty("借机人")
+    private String borrowUserCode;
+
+    @ApiModelProperty("借机人名称")
+    private String borrowUserName;
+
+    @ApiModelProperty("厂别")
+    private String factoryName;
+
+    @ApiModelProperty("设备群组")
+    private String area;
+
+    @ApiModelProperty("线体")
+    private String eqpId;
+
+    @ApiModelProperty("设备")
+    private String unitId;
+
+    @ApiModelProperty("停线类型：1=Line，2=Unit(不停线)")
+    private String stopLineType;
+
+    @ApiModelProperty("借机时间")
+    private LocalDateTime borrowTime;
+
+    @ApiModelProperty("计划借机时长")
+    private BigDecimal borrowPlanHours;
+
+    @ApiModelProperty("班别：1=白班，2=夜班")
+    private String shiftType;
+
+    @ApiModelProperty("借机模式：1=普通借机，2=紧急借机")
+    private String borrowMode;
+
+    @ApiModelProperty("借机类型：ENG=工程借机，F_ENG=实验借机，PM=保养借机，EQ_D=宕机保养")
+    private String borrowType;
+
+    @ApiModelProperty("借机原因")
+    private String borrowReason;
+
+    @ApiModelProperty("主管用户code")
+    private String leaderUserCode;
+
+    @ApiModelProperty("生产计划用户code")
+    private String productPlanUserCode;
+
+    @ApiModelProperty("主管用户名称")
+    private String leaderUserName;
+
+    @ApiModelProperty("生产计划用户名称")
+    private String productPlanUserName;
+
+    @ApiModelProperty("子设备")
+    private String subUnitId;
+
+    @ApiModelProperty("项目列表")
+    private List<EquipmentItemVo> itemList;
+
+}
