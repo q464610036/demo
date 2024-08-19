d8747437847216d035aaf639688279871627b010
chenmengfei1986 <464610036@qq.com>
2024-08-19 12:53:14 +0800

    无

---
+++ spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/dto/EquipmentPTemplateQueryPageDto.java
@@ -0,0 +1,33 @@
+package org.jeecg.modules.equipment.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.jeecg.modules.common.entity.BaseDto;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 点位模版表
+ * </p>
+ *
+ * @author baomidou
+ * @since 2024-07-24
+ */
+@Data
+public class EquipmentPTemplateQueryPageDto extends BaseDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty("模版名称")
+    private String templateName;
+
+    @ApiModelProperty("设备类型")
+    private String unitType;
+
+    @ApiModelProperty(value = "父节点id", hidden = true)
+    private String parentId;
+
+    @ApiModelProperty("设备")
+    private String unitId;
+}
