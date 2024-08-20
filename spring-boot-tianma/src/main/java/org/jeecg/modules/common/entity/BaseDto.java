824ebcb9b003aae5aafce190f6b91babf87dda59
chenmengfei1986 <464610036@qq.com>
2024-08-20 16:24:51 +0800

    无

--- spring-boot-tianma/src/main/java/org/jeecg/modules/common/entity/BaseDto.java
+++ spring-boot-tianma/src/main/java/org/jeecg/modules/common/entity/BaseDto.java
@@ -1,4 +1,12 @@
 package org.jeecg.modules.common.entity;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
 public class BaseDto {
+    @ApiModelProperty("当前页")
+    private Integer pageNo;
+    @ApiModelProperty("每页显示记录数")
+    private Integer pageSize;
 }
