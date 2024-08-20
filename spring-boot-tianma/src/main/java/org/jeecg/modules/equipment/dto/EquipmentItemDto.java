cbd135c068fd2d51148b470628a968eb23afaabb
chenmengfei1986 <464610036@qq.com>
2024-08-20 14:07:59 +0800

    无

--- spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/dto/EquipmentItemDto.java
+++ spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/dto/EquipmentItemDto.java
@@ -2,8 +2,10 @@ package org.jeecg.modules.equipment.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.jeecg.modules.equipment.vo.EquipmentFileVo;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * <p>
@@ -36,5 +38,6 @@ public class EquipmentItemDto implements Serializable {
     @ApiModelProperty("最小周期(W:周,M:月,D:双月,Q:季度,H:半年,Y:年,T:临时,N:宕机)")
     private String minCycle;
 
-
+    @ApiModelProperty("注意事项文件列表")
+    private List<EquipmentFileVo> noteFileList;
 }
