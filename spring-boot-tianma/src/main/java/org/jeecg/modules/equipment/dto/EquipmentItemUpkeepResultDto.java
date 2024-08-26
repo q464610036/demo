8a2ea1d3bf78a657794686848e3b091fa32241bc
chenmengfei1986 <464610036@qq.com>
2024-08-26 16:30:53 +0800

    无

--- spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/dto/EquipmentItemUpkeepResultDto.java
+++ spring-boot-tianma/src/main/java/org/jeecg/modules/equipment/dto/EquipmentItemUpkeepResultDto.java
@@ -23,6 +23,18 @@ public class EquipmentItemUpkeepResultDto implements Serializable {
     @ApiModelProperty("模板保养项目id")
     private String itemId;
 
+    @ApiModelProperty("项目内容")
+    private String itemContent;
+
+    @ApiModelProperty("保养标准")
+    private String standard;
+
+    @ApiModelProperty("注意事项")
+    private String note;
+
+    @ApiModelProperty("最小周期(W:周,M:月,D:双月,Q:季度,H:半年,Y:年,T:临时,N:宕机)")
+    private String minCycle;
+
     @ApiModelProperty("设备")
     private String unitId;
 
