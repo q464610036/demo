efd0b78568a9962d6e9d40ea46a24cf0ebaa70d1
chenmengfei1986 <464610036@qq.com>
2024-09-20 20:13:31 +0800

    无

--- spring-boot-tianma/src/main/resources/设备管理系统4-借机迭代、异常模块表结构.sql
+++ spring-boot-tianma/src/main/resources/设备管理系统4-借机迭代、异常模块表结构.sql
@@ -1,5 +1,21 @@
+-- modify 2024-9-3
+ALTER TABLE ct_tmos_equipment_borrow ADD BORROW_USER_PHONE VARCHAR2(30) NULL;
+COMMENT ON COLUMN ct_tmos_equipment_borrow.BORROW_USER_PHONE IS '申请人联系方式';
+
+ALTER TABLE ct_tmos_equipment_borrow_unit ADD sub_unit_id VARCHAR2(500) NULL;
+COMMENT ON COLUMN ct_tmos_equipment_borrow_unit.sub_unit_id IS '子设备，多个用逗号隔开';
+
+ALTER TABLE ct_tmos_equipment_borrow MODIFY unit_id VARCHAR2(500) NULL;
+COMMENT ON COLUMN ct_tmos_equipment_borrow.unit_id IS '设备，多个用逗号隔开';
+
+ALTER TABLE ct_tmos_equipment_order MODIFY unit_id VARCHAR2(500) NULL;
+COMMENT ON COLUMN ct_tmos_equipment_order.unit_id IS '设备，多个用逗号隔开';
+
+ALTER TABLE CT_TMOS_EQUIPMENT_TEMP_UPKEEP MODIFY unit_id VARCHAR2(500) NULL;
+COMMENT ON COLUMN CT_TMOS_EQUIPMENT_TEMP_UPKEEP.unit_id IS '设备，多个用逗号隔开';
 
 
+-- 异常模块
 CREATE TABLE CT_TMOS_EQUIPMENT_EXCEPTION(
                                             ID VARCHAR2(64) NOT NULL,
                                             EXCEPTION_NO VARCHAR2(32) NOT NULL,
