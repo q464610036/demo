4262f12c04d6b296441da475cdeb9c8a87e97d5b
chenmengfei1986 <464610036@qq.com>
2025-05-31 10:35:10 +0800

    ai ocr识别

---
+++ rabbitMQ-client/src/test/java/com/example/rabbitmqclient/RabbitMqClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
