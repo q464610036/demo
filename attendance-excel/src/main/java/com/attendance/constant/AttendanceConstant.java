ce877ff92d17402bc7fac0f2625c71aff40e03c6
chenmengfei <464610036@qq.com>
2026-04-10 15:30:59 +0800

    变量常量化

--- attendance-excel/src/main/java/com/attendance/constant/AttendanceConstant.java
+++ attendance-excel/src/main/java/com/attendance/constant/AttendanceConstant.java
@@ -1,6 +1,7 @@
 package com.attendance.constant;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class AttendanceConstant {
     public final static LocalTime MORNING_START = LocalTime.of(8, 30);
@@ -8,8 +9,9 @@ public class AttendanceConstant {
     public final static LocalTime AFTERNOON_START = LocalTime.of(13, 30);
     public final static LocalTime AFTERNOON_END = LocalTime.of(17, 30);
 
-    public final static String MORNING_START_STR = "08:30";
-    public final static String MORNING_END_STR = "11:30";
-    public final static String AFTERNOON_START_STR = "13:30";
-    public final static String AFTERNOON_END_STR = "17:30";
+    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+    public final static String MORNING_START_STR = MORNING_START.format(formatter);
+    public final static String MORNING_END_STR = MORNING_END.format(formatter);
+    public final static String AFTERNOON_START_STR = AFTERNOON_START.format(formatter);
+    public final static String AFTERNOON_END_STR = AFTERNOON_END.format(formatter);
 }
